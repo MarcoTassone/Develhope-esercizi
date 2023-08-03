da32d2eca1880088972a0882e966fe5c22813ba5
Marco <tassonemarco67@gmail.com>
2023-08-03 19:44:05 +0200

    added objects methods exercise

---
+++ Objects Methods/exercise/es-23/exercise.js
@@ -0,0 +1,15 @@
+
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+let keys = Object.keys(person)
+let values = Object.values(person)
+
+console.log(keys, values)
+
+// devo dire che la consegna non è molto chiara, da una parte dice di printare le keys e dall'altra i values
+// quindi nel dubbio li ho printati entrambi
+// Print values of person using Object.keys
