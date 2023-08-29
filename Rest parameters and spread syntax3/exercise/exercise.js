c4bc2f3959e6df884950b77add1f00ebb5b2dd10
Marco <tassonemarco67@gmail.com>
2023-08-29 17:57:18 +0200

    added comment

--- Rest parameters and spread syntax3/exercise/exercise.js
+++ Rest parameters and spread syntax3/exercise/exercise.js
@@ -5,6 +5,8 @@ const person = {
   age: 25
 };
 
+//la consegna dice "refactoring", ma secondo me era meglio "destracturing" con rest operator
+
 const {id, ...personInfo} = person;
 
 console.log(id, personInfo);
