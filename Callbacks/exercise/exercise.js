6ac27e597f351d1550f92ce7d4e050e25d752736
Marco <tassonemarco67@gmail.com>
2023-09-06 10:01:28 +0200

    added Callbacks exercise

---
+++ Callbacks/exercise/exercise.js
@@ -0,0 +1,11 @@
+
+function printAsyncName (callback, string){
+    setTimeout(() => callback(), 1000);
+    setTimeout(() => console.log(string),  2000);
+}
+
+function callback(){
+    console.log("Hello");
+}
+
+printAsyncName(callback, "Marco");
